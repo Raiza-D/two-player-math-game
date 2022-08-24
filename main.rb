c5888248ac17429081b86ad15115c36ac84d9b7f
Raiza De Guzman <santos_dg7@hotmail.com>
2022-08-24 12:58:49 -0700

    Add code to instantiate new Game class. Invoke start_game method within Game instance. Add welcome message. Change puts to print for string asking players' names

--- main.rb
+++ main.rb
@@ -1,25 +1,28 @@
 require "./player.rb"
 require "./game.rb"
 
-puts "Player 1 - Enter your name: "
+puts "WELCOME TO MATH-MANIA"
+print "Player 1 - Enter your name: "
 player_one_name = gets.chomp
 player1 = Player.new(player_one_name)
 
-puts "Player 2 - Enter your name: "
+print "Player 2 - Enter your name: "
 player_two_name = gets.chomp
 player2 = Player.new(player_two_name)
 
+game = Game.new(player1, player2)
+game.start_game
 
 # Question
-puts "What does 5 plus 3 equal?"
+# puts "What does 5 plus 3 equal?"
 
-# Capturing user's answer - an input
-answer = gets.chomp
+# # Capturing user's answer - an input
+# answer = gets.chomp
 
-# Message if answer correct or wrong
+# # Message if answer correct or wrong
 
-# Score (lives remaining) for each player
-puts "P1: vs P2: "
+# # Score (lives remaining) for each player
+# puts "P1: vs P2: "
 
-# New Turn divider
-puts "----- NEW TURN -----"
+# # New Turn divider
+# puts "----- NEW TURN -----"
