e1c9bd78df27fdb18045948a2cd0c20b9dc5fcd6
Raiza De Guzman <santos_dg7@hotmail.com>
2022-08-24 12:39:09 -0700

    Add conditionals to check if user's answer to math q correct and print appropriate msg. Add function to check if game is over by adding conditional to check if player lives is zero

--- game.rb
+++ game.rb
@@ -12,6 +12,20 @@ class Game
 
     puts "#{player.name}: What is #{first_num} plus #{second_num}?"
     @answer = gets.chomp.to_i
+
+    if (@first_num + @second_num == @answer)
+      puts "Correct, you're a math genius!"
+    else
+      puts "Seriously? That's incorrect."
+    end
+
+    if (player.lives == 0)
+      @game_over = game_over(player)
+    end
+  end
+
+  def game_over?(player)
+    puts "Game over. #{player.name} lost."
   end
 
 end
