504d344f8f2510ca5ac8f937bb5090f7ce8b5323
Raiza De Guzman <santos_dg7@hotmail.com>
2022-08-24 12:20:42 -0700

    Add initialize method. Add method to run with math question and captures player's answer

--- game.rb
+++ game.rb
@@ -1,10 +1,17 @@
 class Game
-  attr_accessor :player1 :player2
-
-  def start_game
+  def initialize(player1, player2)
+    @player1 = player1
+    @player2 = player2
+    @game_over = false
+    @player1_turn = true
   end
-  
-  def new_turn
+
+  def game_question(player)
+    @first_num = rand(20)
+    @second_num = rand(20)
+
+    puts "#{player.name}: What is #{first_num} plus #{second_num}?"
+    @answer = gets.chomp.to_i
   end
 
 end
